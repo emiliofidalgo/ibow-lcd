c6b46dda8a3aa365c875f6172c408c54dd7d7bbe
Emilio Garcia-Fidalgo <emilio.garcia@uib.es>
2017-10-31 17:26:08 +0100

    First main file

---
+++ src/main.cc
@@ -0,0 +1,25 @@
+/**
+* This file is part of ibow-lcd.
+*
+* Copyright (C) 2017 Emilio Garcia-Fidalgo <emilio.garcia@uib.es> (University of the Balearic Islands)
+*
+* ibow-lcd is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* ibow-lcd is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with ibow-lcd. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <iostream>
+
+int main() {
+  std::cout << "Hello World" << std::endl;
+  return 0;
+}
