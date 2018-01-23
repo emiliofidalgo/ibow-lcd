532f84c8b7789757198ddafb9122f7d3cea79ca5
Emilio Garcia-Fidalgo <emilio.garcia@uib.es>
2018-01-23 12:59:01 +0100

    Adding KITTI05 configurations

--- evaluation/matlab/main.m
+++ evaluation/matlab/main.m
@@ -22,6 +22,9 @@ curr_dir = strcat(base_dir, 'Lip6Out/');
 curr_dir = strcat(base_dir, 'KITTI00/');
 [PR_K0, imgvssize_K0, imgvstime_K0] = process(curr_dir, gt_neigh, compensate);
 
+curr_dir = strcat(base_dir, 'KITTI05/');
+[PR_K5, imgvssize_K5, imgvstime_K5] = process(curr_dir, gt_neigh, compensate);
+
 % P/R curves
 afigure;
 hold on;
@@ -30,11 +33,12 @@ plot(PR_NC.R, PR_NC.P, '-*', 'MarkerIndices', length(PR_NC.P));
 plot(PR_L6I.R, PR_L6I.P, '-x', 'MarkerIndices', length(PR_L6I.P));
 plot(PR_L6O.R, PR_L6O.P, '--s', 'MarkerIndices', length(PR_L6O.P));
 plot(PR_K0.R, PR_K0.P, '-d', 'MarkerIndices', length(PR_K0.P));
+plot(PR_K5.R, PR_K5.P, '--^', 'MarkerIndices', length(PR_K5.P));
 xlabel('Recall');
 ylabel('Precision');
 xlim([0., 1.02]);
 ylim([0., 1.02]);
-legend('CC', 'NC', 'L6I', 'L6O', 'K00', 'Location', 'SouthWest');
+legend('CC', 'NC', 'L6I', 'L6O', 'K00', 'K05', 'Location', 'SouthWest');
 hold off;
 print('-depsc', strcat(base_dir, 'PR_curves'));
 
@@ -46,9 +50,10 @@ plot(imgvssize_NC.img, imgvssize_NC.size, '-*', 'MarkerIndices', length(imgvssiz
 plot(imgvssize_L6I.img, imgvssize_L6I.size, '-*', 'MarkerIndices', length(imgvssize_L6I.size));
 plot(imgvssize_L6O.img, imgvssize_L6O.size, '--s', 'MarkerIndices', length(imgvssize_L6O.size));
 plot(imgvssize_K0.img, imgvssize_K0.size, '-d', 'MarkerIndices', length(imgvssize_K0.size));
+plot(imgvssize_K5.img, imgvssize_K5.size, '--^', 'MarkerIndices', length(imgvssize_K5.size));
 xlabel('Time Index');
 ylabel('Vocabulary Size (Words)');
-legend('CC', 'NC', 'L6I', 'L6O', 'K00', 'Location', 'NorthWest');
+legend('CC', 'NC', 'L6I', 'L6O', 'K00', 'K05', 'Location', 'NorthWest');
 hold off;
 print('-depsc', strcat(base_dir, 'imgs_vs_size'));
 
@@ -60,9 +65,10 @@ plot(imgvstime_NC.img, imgvstime_NC.time, '-*', 'MarkerIndices', length(imgvstim
 plot(imgvstime_L6I.img, imgvstime_L6I.time, '-*', 'MarkerIndices', length(imgvstime_L6I.time));
 plot(imgvstime_L6O.img, imgvstime_L6O.time, '--s', 'MarkerIndices', length(imgvstime_L6O.time));
 plot(imgvstime_K0.img, imgvstime_K0.time, '-d', 'MarkerIndices', length(imgvstime_K0.time));
+plot(imgvstime_K5.img, imgvstime_K5.time, '--^', 'MarkerIndices', length(imgvstime_K5.time));
 xlabel('Time Index');
 ylabel('Avg. Time (ms)');
-legend('CC', 'NC', 'L6I', 'L6O', 'K00', 'Location', 'NorthWest');
+legend('CC', 'NC', 'L6I', 'L6O', 'K00', 'K05', 'Location', 'NorthWest');
 hold off;
 print('-depsc', strcat(base_dir, 'imgs_vs_time'));
 
@@ -101,5 +107,12 @@ disp(['Max R: ', num2str(PR_K0.R_max)]);
 disp(['Max VWords: ', num2str(imgvssize_K0.size(end))]);
 disp(['Avg. Time: ', num2str(mean(imgvstime_K0.time))]);
 disp(['Std. Time: ', num2str(std(imgvstime_K0.time))]);
+
+disp('----- Summary KITTI 05 -----');
+disp(['Max P: ', num2str(PR_K5.P_max)]);
+disp(['Max R: ', num2str(PR_K5.R_max)]);
+disp(['Max VWords: ', num2str(imgvssize_K5.size(end))]);
+disp(['Avg. Time: ', num2str(mean(imgvstime_K5.time))]);
+disp(['Std. Time: ', num2str(std(imgvstime_K5.time))]);
 
 close all;
